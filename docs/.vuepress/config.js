34bab833f3499b7ae7b5fcbb2243c6e24199f38a
hackycy <qa894178522@qq.com>
2021-10-11 10:35:23 +0800

    chore: 更改vuepress标题配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,8 @@
 'use strict'
 
 module.exports = {
-  base: '/sf-admin-cli/',
-  title: 'SF-ADMIN',
-  description: '快速、准确、方便的创建 sf-admin 项目',
+  title: 'Ark Link开源官方文档',
+  description: '',
   themeConfig: {
     search: false,
     lastUpdated: '最后更新于',
